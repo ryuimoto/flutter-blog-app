ed31e048e5449ec6ecbc91a61c3be519bfd2c374
maca <narrow617@gmail.com>
2022-06-08 18:42:54 +0900

    add user model

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:blog_app/screens/loading.dart';
 
 void main(){
   runApp(App());
 }
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
-  Widget Build(BuildContext context){
+  Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Loading(),

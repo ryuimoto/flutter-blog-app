b528d98196ddf532e6a7d1ab5ea1144952e9ad38
maca <narrow617@gmail.com>
2022-06-08 18:53:35 +0900

    e

---
+++ lib/constant.dart
@@ -0,0 +1,12 @@
+// --------- STRINGS ---------
+const baseURL = 'http://localhost:99/api';
+const loginURL = baseURL + '/login';
+const registerURL = baseURL + '/register';
+const logoutURL = baseURL + '/logout';
+const userURL = baseURL + '/user';
+const postsURL = baseURL + '/posts';
+const commentsURL = baseURL + '/comments';
+
+const serverError = 'Server error';
+const unauthorized = 'Unauthorized';
+const somethingWentWrong = 'something went wrong, try again!';

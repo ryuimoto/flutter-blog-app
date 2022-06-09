fc184ca0811a2d517abaa508e7be4359c2932c36
maca <narrow617@gmail.com>
2022-06-09 16:41:44 +0900

    add form

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -6,10 +6,37 @@ class Login extends StatefulWidget{
 }
 
 class _LoginState extends State<Login>{
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController txtPassword = TextEditingController();
+
+
+
+
   @override
   Widget build(BuildContext context){
-    return Container(
-
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: EdgeInsets.all(32),
+          children: [
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: txtEmail,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                contentPadding: EdgeInsets.all(10),
+                border: OutlineInputBorder(borderSide: BorderSide(width: 1,color: Colors.black)),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

691851e1fd017b21c6584866803fee5c837ee117
maca <narrow617@gmail.com>
2022-06-09 15:30:04 +0900

    e login.dart

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,6 @@
+import 'package:blog_app/services/user_service.dart';
 import 'package:flutter/material.dart';
+import 'package:blog_app/screens/login.dart';
 
 class Loading extends StatefulWidget{
   @override
@@ -6,6 +8,15 @@ class Loading extends StatefulWidget{
 }
 
 class _LoadingState extends State<Loading>{
+
+  void _loadUserInfo() async{
+    String token = await getToken();
+    if(token == ''){
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => Login()), (route) => false);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context){
     return Container(

fb4b93cd34bd0d74aa5289503a0aef6c79c9ef3e
maca <narrow617@gmail.com>
2022-06-13 16:10:42 +0900

    add form

--- lib/screens/post_form.dart
+++ lib/screens/post_form.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/constant.dart';
 import 'package:flutter/material.dart';
 
 class PostForm extends StatefulWidget{
@@ -6,13 +7,18 @@ class PostForm extends StatefulWidget{
 }
 
 class _PostFormState extends State<PostForm>{
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _txtControllerBody = TextEditingController();
+  bool _loading = false;
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Add new post'),
       ),
-      body: ListView(
+      body: _loading ? Center(child: CircularProgressIndicator(),) : ListView(
         children: [
           Container(
             width: MediaQuery.of(context).size.width,
@@ -27,13 +33,31 @@ class _PostFormState extends State<PostForm>{
             ),
           ),
           Form(
+            key: _formKey,
+
             child: Padding(
               padding: EdgeInsets.all(8),
               child: TextFormField(
+                controller: _txtControllerBody,
                 keyboardType: TextInputType.multiline,
-                
+                maxLines: 9,
+                validator: (val) => val!.isEmpty ? 'Post body is required' : null,
+                decoration: InputDecoration(
+                  hintText: 'Post body...',
+                  border: OutlineInputBorder(borderSide: BorderSide(width: 1,color: Colors.black38)),
+                ),
               ),
             ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: kTextButton('Post', (){
+              if(_formKey.currentState!.validate()){
+                setState((){
+                  _loading = !_loading;
+                });
+              }
+            }),
           ),
         ],
       ),

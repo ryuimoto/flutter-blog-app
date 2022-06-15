15c55e478d9cbcd4ce0947c75ed8ba6dd7362fda
maca <narrow617@gmail.com>
2022-06-15 18:27:29 +0900

    edit screen

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -4,7 +4,6 @@ import 'package:blog_app/screens/login.dart';
 import 'package:blog_app/services/post_service.dart';
 import 'package:blog_app/services/user_service.dart';
 import 'package:flutter/material.dart';
-
 import '../models/post.dart';
 
 class PostScreen extends StatefulWidget{
@@ -40,7 +39,8 @@ class _PostScreenState extends State<PostScreen>{
 
   @override
   void initState(){
-    
+    retrivePosts();
+    super.initState();
   }
 
   @override
@@ -50,7 +50,72 @@ class _PostScreenState extends State<PostScreen>{
             itemCount: _postList.length,
             itemBuilder: (BuildContext context,int index){
               Post post = _postList[index];
-              return Text('${post.body}');
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 4,vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 6),
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 38,
+                                  height: 38,
+                                  decoration: BoxDecoration(
+                                    image: post.user!.image != null ?
+                                        DecorationImage(image: NetworkImage('${post.user!.image}')) : null,
+                                    borderRadius: BorderRadius.circular(25),
+                                    color: Colors.amber
+                                  ),
+                                ),
+                                SizedBox(width: 10,),
+                                Text(
+                                  '${post.user!.name}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 17,
+                                  ),
+                                ),
+                              ],
+                            ),
+                        ),
+                        post.user!.id == userId ?
+                        PopupMenuButton(
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: Icon(Icons.more_vert,color: Colors.black,),
+                          ),
+                          itemBuilder: (context) => [
+                            PopupMenuItem(
+                              child: Text('Edit'),
+                              value: 'edit',
+                            ),
+                            PopupMenuItem(
+                              child: Text('Delete'),
+                              value: 'delete',
+                            ),
+                          ],
+                          onSelected: (val) =>{
+                            if(val == 'edit'){
+                              // edit
+                            } else{
+                              // delete
+                            }
+                          },
+                        ) : SizedBox(),
+                      ],
+                    ),
+                    SizedBox(height: 12,),
+                    Text('${post.body}'),
+                    post.image != null ?
+                        
+                  ],
+                ),
+              );
             },
         );
   }

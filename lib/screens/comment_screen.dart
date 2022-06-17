959f69362dd3257e5fddcc620ae72d706e3195f5
maca <narrow617@gmail.com>
2022-06-17 18:17:50 +0900

    edit view

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -4,6 +4,8 @@ import 'package:blog_app/screens/login.dart';
 import 'package:blog_app/services/comments_service.dart';
 import 'package:blog_app/services/user_service.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import '../models/comment.dart';
 
 class CommentScreen extends StatefulWidget{
   final int? postId;
@@ -22,15 +24,15 @@ class _CommentScreenState extends State<CommentScreen>{
   int userId = 0;
 
   // Get comments
-  Future<void> _getComemnts() async{
+  Future<void> _getComments() async{
     userId = await getUserId();
     ApiResponse response = await getComments(widget.postId ?? 0);
 
     if(response.error == null){
-      setState(){
+      setState((){
         _commentsList = response.data as List<dynamic>;
         _loading = _loading ? !_loading : _loading;
-      }
+      });
     }
     else if(response.error == unauthorized){
       logout().then((value) => {
@@ -45,11 +47,121 @@ class _CommentScreenState extends State<CommentScreen>{
   }
 
   @override
-  Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Comments'),
-        ),
-      );
+  void initState(){
+    _getComments();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Comments'),
+      ),
+      body: _loading ? Center(child: CircularProgressIndicator(),) :
+      Column(
+          children: [
+            Expanded(
+                child: RefreshIndicator(
+                    onRefresh: (){
+                      return _getComments();
+                    },
+                    child: ListView.builder(
+                        itemCount: _commentsList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          Comment comment = _commentsList[index];
+                          return Container(
+                            padding: EdgeInsets.all(10),
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                border: Border(
+                                    bottom: BorderSide(color: Colors.black26, width: 0.5)
+                                )
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Container(
+                                          width: 30,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                              image: comment.user!.image != null ? DecorationImage(
+                                                  image: NetworkImage('${comment.user!.image}'),
+                                                  fit: BoxFit.cover
+                                              ) : null,
+                                              borderRadius: BorderRadius.circular(15),
+                                              color: Colors.blueGrey
+                                          ),
+                                        ),
+                                        SizedBox(width: 10,),
+                                        Text(
+                                          '${comment.user!.name}',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w600,
+                                              fontSize: 16
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    comment.user!.id == userId ?
+                                    PopupMenuButton(
+                                      child: Padding(
+                                          padding: EdgeInsets.only(right:10),
+                                          child: Icon(Icons.more_vert, color: Colors.black,)
+                                      ),
+                                      itemBuilder: (context) => [
+                                        PopupMenuItem(
+                                            child: Text('Edit'),
+                                            value: 'edit'
+                                        ),
+                                        PopupMenuItem(
+                                            child: Text('Delete'),
+                                            value: 'delete'
+                                        )
+                                      ],
+                                      onSelected: (val){
+                                        if(val == 'edit'){
+                                          setState(() {
+
+                                          });
+
+                                        } else {
+                                        }
+                                      },
+                                    ) : SizedBox()
+                                  ],
+                                ), SizedBox(height: 10,),
+                                Text('${comment.comment}')
+                              ],
+                            ),
+                          );
+                        }
+                    )
+                )
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                border: Border(
+                    top: BorderSide(color: Colors.black26, width: 0.5
+                    )
+                ),
+              ),
+              child: Row(
+                children: [
+
+
+                ],
+              ),
+            )
+          ]
+      ),
+    );
   }
 }

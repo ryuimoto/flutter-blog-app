48191f7bbf4de9d3a57fc50ca04f90847473bffd
maca <narrow617@gmail.com>
2022-06-17 13:27:42 +0900

    add delete comment

--- lib/services/comments_service.dart
+++ lib/services/comments_service.dart
@@ -4,6 +4,7 @@ import 'package:blog_app/constant.dart';
 import 'package:blog_app/services/user_service.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import '../models/comment.dart';
 
 Future<ApiResponse> getComments(int postId) async{
   ApiResponse apiResponse = ApiResponse();
@@ -18,8 +19,79 @@ Future<ApiResponse> getComments(int postId) async{
 
     switch(response.statusCode){
       case 200:
+        // map each comemnts to comment model
         apiResponse.data = jsonDecode(response.body)['comments'].map((p) => Comment.fromJson(p)).toList();
         apiResponse.data as List<dynamic>;
+        break;
+      case 402:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWentWrong;
+        break;
+    }
+  }
+  catch(e){
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+//Create comment
+Future<ApiResponse> createComments(int postId,String? comment) async{
+  ApiResponse apiResponse = ApiResponse();
+
+  try{
+    String token = await getToken();
+    final response = await http.post(Uri.parse('$postsURL/$postId/comments'),
+        headers: {
+          'Accept' : 'application/json',
+          'Authorization' : 'Bearer $token',
+        },body: {
+          'comment' : comment,
+        });
+
+    switch(response.statusCode){
+      case 200:
+      // map each comemnts to comment model
+        apiResponse.data = jsonDecode(response.body)['comments'].map((p) => Comment.fromJson(p)).toList();
+        apiResponse.data as List<dynamic>;
+        break;
+      case 402:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWentWrong;
+        break;
+    }
+  }
+  catch(e){
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+//Delete comment
+Future<ApiResponse> deleteComments(int commentId) async{
+  ApiResponse apiResponse = ApiResponse();
+
+  try{
+    String token = await getToken();
+    final response = await http.delete(Uri.parse('$commentsURL/$commentId'),
+        headers: {
+          'Accept' : 'application/json',
+          'Authorization' : 'Bearer $token',
+        });
+
+    switch(response.statusCode){
+      case 200:
+        apiResponse.data = jsonDecode(response.body)['message'];
         break;
       case 402:
         apiResponse.error = jsonDecode(response.body)['message'];

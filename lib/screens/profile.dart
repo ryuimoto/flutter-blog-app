f8893f22b20611cc1f80120660b24c243dcc6ee5
maca <narrow617@gmail.com>
2022-06-19 11:53:18 +0900

    rename profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,15 +1,58 @@
+import 'dart:html';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import '../models/user.dart';
 
 class Profile extends StatefulWidget{
   @override
-  _ProfileScreenState createState() => _ProfileScreenState();
+  _ProfileState createState() => _ProfileState();
 }
 
-class _ProfileScreenState extends State<Profile>{
+class _ProfileState extends State<Profile>{
+  User? user;
+  bool loading = true;
+  File? _imageFile;
+  final _picker = ImagePicker();
+
+  Future getImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if(pickedFile != null){
+      setState((){
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+
   @override
   Widget build(BuildContext context){
-    return Container(
-      child: Center(child: Text('Profile'),),
-    );
+    return loading ? Center(child: CircularProgressIndicator(),) :
+        Padding(
+          padding: EdgeInsets.only(top: 40,left: 40,right: 40),
+          child: ListView(
+            children: [
+              Center(
+                child: GestureDetector(
+                  child: Container(
+                    width: 110,
+                    height: 110,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(60),
+                      image: _imageDile == null ? user!.image != null ? DecorationImage(
+                        image: NetworkImage('${user!.image}'),
+                        fit: BoxFit.cover
+                      ) : null : DecorationImage(
+                          image: FileImage(_imageFile ?? File('')),
+                          fit: BoxFit.cover
+                      ),
+                      color: Colors.amber
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
   }
 }
